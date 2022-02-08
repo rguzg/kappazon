0765a7d10c18c62d74fa0b3e549a962179de718e
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-08 13:28:49 -0600

    feat(LogIn_CreateAccount): Hide App Buttons if not logged in

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,12 @@ import { Container, Col } from 'react-bootstrap';
 import kappazon from '../kappazon.jpeg';
 import cart from '../Icons/cart.svg';
 import users from '../Icons/users.svg';
+import { useContext } from 'react';
+import { UserContext } from '../context';
 
 export default function Header() {
+  const { isLoggedIn } = useContext(UserContext);
+
   return (
     <Container fluid>
       <Col xs={12} className="App-header">
@@ -13,24 +17,26 @@ export default function Header() {
             <h2>Kappazon</h2>
           </div>
         </Col>
-        <Col xs={3}>
-          <div className="App-buttons">
-            <img
-              src={cart}
-              alt="kappazon"
-              height={50}
-              width={50}
-              style={{ padding: '0 8px' }}
-            />
-            <img
-              src={users}
-              alt="kappazon"
-              height={50}
-              width={50}
-              style={{ padding: '0 8px' }}
-            />
-          </div>
-        </Col>
+        {isLoggedIn && (
+          <Col xs={3}>
+            <div className="App-buttons">
+              <img
+                src={cart}
+                alt="kappazon"
+                height={50}
+                width={50}
+                style={{ padding: '0 8px' }}
+              />
+              <img
+                src={users}
+                alt="kappazon"
+                height={50}
+                width={50}
+                style={{ padding: '0 8px' }}
+              />
+            </div>
+          </Col>
+        )}
       </Col>
     </Container>
   );
