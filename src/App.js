78ee747ffd3b19f02d32b5207d22f345c566ca0c
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-08 13:28:27 -0600

    feat(LogIn_CreateAccount): User isLoggedIn property is now managed as a state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
 import Header from './Components/Header';
+import Login_CreateAccount from './Components/Login_CreateAccount';
 import { UserContext, User } from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
   const [user, setUser] = useState(new User());
+  const [isLoggedIn, setLogIn] = useState(user.isLoggedIn);
 
   return (
     <div className="App">
       <UserContext.Provider
         value={{
           user,
+          updateUser: (newUser) => {
+            setUser(newUser);
+            setLogIn(newUser.isLoggedIn);
+          },
+          isLoggedIn: isLoggedIn,
+          login: user.login,
           accessToken: user.accessToken,
           refreshToken: user.refreshToken,
         }}
       >
         <Header />
+        {isLoggedIn ? <div>Logged in</div> : <Login_CreateAccount />}
       </UserContext.Provider>
     </div>
   );
